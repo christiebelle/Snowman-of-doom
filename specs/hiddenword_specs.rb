443a814a95c385eaa1a199aa8ec6e5ce29807de3
Christiebelle <cslester@outlook.com>
2018-01-31 13:17:16 +0000

    MVP basics

--- specs/hiddenword_specs.rb
+++ specs/hiddenword_specs.rb
@@ -5,4 +5,16 @@ require_relative("../hiddenword.rb")
 
 class HiddenWordTest < MiniTest::Test
 
+  def setup
+    @word = HiddenWord.new("The Avengers")
+  end
+
+  def test_returns_word
+    assert_equal("The Avengers", @word.returns_word)
+  end
+
+  def test_redaction
+    assert_equal("*** ********", @word.redaction)
+  end
+
 end
